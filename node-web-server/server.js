b5465c0a4fdea6aff74d090a292764e2cdfde0ee
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-18 20:00:30 +0200

    lecture 44 - end

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,21 +2,28 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    year: new Date().getFullYear(),
     welcomeMessage: 'Witaj na stronie domowej!'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    year: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
