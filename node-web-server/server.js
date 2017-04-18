7d9a73ea07cf9a4cd7622f0b0f91081fc50f611c
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-18 19:20:21 +0200

    Home page.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,23 @@
 const express = require('express');
-
+const hbs = require('hbs');
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', (req, res) => {
-  //res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Pawel',
-    likes:[
-      'snowboard',
-      'programming'
-    ]
-  });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    year: new Date().getFullYear(),
+    welcomeMessage: 'Witaj na stronie domowej!'
+  })
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page!');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    year: new Date().getFullYear()
+  });
 });
 
 app.get('/bad',(req,res) => {
