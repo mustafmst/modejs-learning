e3be2d2861a6687be58007d4adcf572a8e1bb08b
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-18 18:44:11 +0200

    Add /bad route.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,6 +3,24 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express!</h1>');
+  //res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Pawel',
+    likes:[
+      'snowboard',
+      'programming'
+    ]
+  });
 });
+
+app.get('/about', (req, res) => {
+  res.send('About page!');
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage: 'Woops! Coś się zjebało :P'
+  });
+});
+
 app.listen(3000);
