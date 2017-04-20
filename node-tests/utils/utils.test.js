f231a8c5b200ef9f81b0168b1b7c5378151a1d98
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-20 18:52:25 +0200

    Lecture 56 - end

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -3,14 +3,26 @@ const expect = require('expect');
 
 it('should add two numbers', () => {
   var res = utils.add(3,4);
-
   expect(res).toExist().toBeA('number').toBe(7);
+});
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(3,4,(sum) => {
+    expect(sum).toBeA('number').toBe(7);
+    done();
+  });
 });
 
 it('should square a number', () => {
   var res = utils.square(10);
-
   expect(res).toExist().toBeA('number').toBe(100);
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(3, (square) => {
+    expect(square).toBeA('number').toBe(9);
+    done();
+  });
 });
 
 it('shoult expect some values', () => {
