34a52a45d43d20ed178ea487289c875148812b32
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-20 18:18:25 +0200

    Lecture 53 - end

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,35 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
   var res = utils.add(3,4);
-  
-  if(res !== 7){
-    throw new Error(`Expected 7 but was ${res}`);
-  }
+
+  expect(res).toExist().toBeA('number').toBe(7);
 });
 
-it('should square number', () => {
+it('should square a number', () => {
   var res = utils.square(10);
 
-  if(res !== 100){
-    throw new Error(`Expected 100 but was ${res}`);
-  }
+  expect(res).toExist().toBeA('number').toBe(100);
+});
+
+it('shoult expect some values', () => {
+  expect(12).toNotBe(9);
+  expect({name: "Pawel"}).toNotEqual({name:"pawel"});
+  expect([2,3,4]).toExclude(5);
+  expect({
+    name: 'Pawel',
+    age: 24,
+    location: 'Krakow'
+  }).toInclude({
+    age: 24
+  });
+});
+
+it('should return valid user object', () => {
+  var user = utils.setName({}, "Paweł Mstowski");
+  expect(user)
+    .toBeA('object')
+    .toInclude({firstName: "Paweł"})
+    .toInclude({lastName: "Mstowski"});
 });
