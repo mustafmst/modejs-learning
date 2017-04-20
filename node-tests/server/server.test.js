648abec02eb3d0e305dd386dd53cf9d7e9ddd6fe
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-20 19:56:45 +0200

    Lecture 58 - end

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,36 +3,42 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+      .get('/')
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          error: 'Page not found.'
+        });
+      })
+      .end(done);
+    });
 });
 
-it('should return users array', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body)
-        .toInclude({
-          name: 'Paweł'
-        })
-        .toInclude({
-          name: 'Ania'
-        })
-        .toInclude({
-          name: 'Michał'
-        })
-        .toInclude({
-          name: 'Jacek'
-        });
-    })
-    .end(done);
+describe('Get /users', () => {
+  it('should return users array', (done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body)
+          .toInclude({
+            name: 'Paweł'
+          })
+          .toInclude({
+            name: 'Ania'
+          })
+          .toInclude({
+            name: 'Michał'
+          })
+          .toInclude({
+            name: 'Jacek'
+          });
+      })
+      .end(done);
+    });
+  });
 });
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,47 +1,41 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-  var res = utils.add(3,4);
-  expect(res).toExist().toBeA('number').toBe(7);
-});
+describe('Utils', () => {
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(3,4);
+      expect(res).toExist().toBeA('number').toBe(7);
+    });
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(3,4,(sum) => {
-    expect(sum).toBeA('number').toBe(7);
-    done();
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(3,4,(sum) => {
+        expect(sum).toBeA('number').toBe(7);
+        done();
+      });
+    });
   });
-});
 
-it('should square a number', () => {
-  var res = utils.square(10);
-  expect(res).toExist().toBeA('number').toBe(100);
-});
+  describe('#square', () => {
+    it('should square a number', () => {
+      var res = utils.square(10);
+      expect(res).toExist().toBeA('number').toBe(100);
+    });
 
-it('should async square a number', (done) => {
-  utils.asyncSquare(3, (square) => {
-    expect(square).toBeA('number').toBe(9);
-    done();
+    it('should async square a number', (done) => {
+      utils.asyncSquare(3, (square) => {
+        expect(square).toBeA('number').toBe(9);
+        done();
+      });
+    });
   });
-});
 
-it('shoult expect some values', () => {
-  expect(12).toNotBe(9);
-  expect({name: "Pawel"}).toNotEqual({name:"pawel"});
-  expect([2,3,4]).toExclude(5);
-  expect({
-    name: 'Pawel',
-    age: 24,
-    location: 'Krakow'
-  }).toInclude({
-    age: 24
-  });
-});
-
-it('should return valid user object', () => {
-  var user = utils.setName({}, "Paweł Mstowski");
-  expect(user)
+  it('should return valid user object', () => {
+    var user = utils.setName({}, "Paweł Mstowski");
+    expect(user)
     .toBeA('object')
     .toInclude({firstName: "Paweł"})
     .toInclude({lastName: "Mstowski"});
+  });
+
 });
