7bab843b9544cb1a70e759d1ab48f5c4ce2c613d
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-22 11:25:02 +0200

    todo post route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+  var todo = new Todo({
+    text: req.body.text
+  }).save()
+    .then((doc) => {
+      console.log(JSON.stringify(doc, undefined, 2));
+      res.status(201).send(doc);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Started server on port: 3000');
+})
