cd01ac67a139081d8b3c39e7ea75d03c71bddfad
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-04-22 12:36:36 +0200

    Get test

--- node-todo-api/tests/server.test.js
+++ node-todo-api/tests/server.test.js
@@ -4,9 +4,17 @@ const expect = require('expect');
 var {app} = require('../server/server');
 var {Todo} = require('../server/models/todo');
 
+const todos = [{
+  text: 'first todo'
+},{
+  text: 'second todo'
+}];
+
 beforeEach((done) => {
   Todo.remove({})
-    .then(() => done());
+    .then(() => {
+      return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -25,7 +33,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find()
+        Todo.find({text})
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -47,11 +55,23 @@ describe('POST /todos', () => {
 
         Todo.find()
           .then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch((err) => done(err));
       });
   });
 
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done);
+  });
 });
