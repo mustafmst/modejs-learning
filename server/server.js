59f579a1062ae0657358e0cdbf8263f2cfe3d47a
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-05-02 13:29:47 +0200

    add tokens

--- server/server.js
+++ server/server.js
@@ -124,8 +124,11 @@ app.post('/users', (req, res) => {
   });
 
   user.save()
-    .then((user) => {
-      res.send({user});
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then((token) => {
+      res.header('x-auth',token).send(user);
     })
     .catch((e) => {
       res.status(400).send(e);
