c2c2494cf7d7f8fb2dad5f292bb5a74e58384290
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-05-02 17:53:20 +0200

    Add login tests

--- tests/server.todo.test.js
+++ tests/server.todo.test.js
@@ -284,7 +284,8 @@ describe('POST /users', () => {
             expect(user).toExist();
             expect(user.password).toNotBe(password);
             done();
-          });
+          })
+          .catch((err) => done(err));
       });
   });
   it('should return validation errors if request invalid', (done) => {
@@ -301,6 +302,54 @@ describe('POST /users', () => {
     request(app)
       .post('/users')
       .send({email: users[0].email,password: users[0].password})
+      .expect(400)
+      .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if(err) return done(err);
+
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          })
+          .catch((err) => done(err));
+      });
+  });
+  it('should return 400 status if password is incorrect', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password+'123'
+      })
+      .expect(400)
+      .end(done);
+  });
+  it('should return 400 status if user do not exist', (done) => {
+    var email = 'lol@notExist.com';
+    var password = 'lol:P:';
+
+    request(app)
+      .post('/users/login')
+      .send({email, password})
       .expect(400)
       .end(done);
   });
