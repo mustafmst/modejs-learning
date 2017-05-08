0c3cd1ccc0a73ea7bdb0e55d31bb26e23520e0b7
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-05-08 21:40:00 +0200

    location template

--- public/js/index.js
+++ public/js/index.js
@@ -22,13 +22,24 @@ socket.on('newMessage', function(message) {
 });
 
 socket.on('newLocationMessage', function(message) {
+
   var formattedTime = moment(message.createdAt).format('H:mm');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  // 
+  // var formattedTime = moment(message.createdAt).format('H:mm');
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My location</a>');
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e) {
