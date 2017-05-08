bf7b353f37cbc1a1f62d62b58aaac339ead583af
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-05-08 21:59:59 +0200

    autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 
@@ -19,10 +35,10 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
-
   var formattedTime = moment(message.createdAt).format('H:mm');
   var template = jQuery('#location-message-template').html();
   var html = Mustache.render(template, {
@@ -32,14 +48,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
-  // 
-  // var formattedTime = moment(message.createdAt).format('H:mm');
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My location</a>');
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
